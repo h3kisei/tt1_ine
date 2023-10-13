858e6d0c767b1c7d8d39635523cc84ebef037228
HHoang <hoanghust218@gmail.com>
2023-10-13 14:16:22 +0700

    hoang

--- script.js
+++ script.js
@@ -27,41 +27,6 @@ function scrolling() {
   imageWrapper.style.transition = ".3s";
 }
 
-const gap = 16;
-
-const carousel = document.getElementById("carousel"),
-  content = document.getElementById("content"),
-  next = document.getElementById("next__share"),
-  prev = document.getElementById("prev__share");
-
-next.addEventListener("click", (e) => {
-  carousel.scrollBy(width + gap, 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "none";
-  }
-});
-
-console.log(carousel.scrollWidth);
-prev.addEventListener("click", (e) => {
-  carousel.scrollBy(-(width + gap), 0);
-  if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = "none";
-  }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
-  }
-});
-
-let width = carousel.offsetWidth;
-window.addEventListener("resize", (e) => (width = carousel.offsetWidth));
-
-function scrollToTop() {
-  window.scrollTo(100, 100);
-}
-
 var nav;
 var menu;
 
