b7e2da8c94b60a5d273b9da956b687e6d5845245
HHoang <hoanghust218@gmail.com>
2023-10-13 14:52:48 +0700

    hoangg

--- script.js
+++ script.js
@@ -61,3 +61,22 @@ $(document).ready(function () {
     },
   });
 });
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
